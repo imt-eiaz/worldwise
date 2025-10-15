3958a64e10e200394906f3b1e466a332dc51c208
imt-eiaz <imteiaz@gmail.com>
2025-10-15 17:38:14 +0100

    10. Nested Routes and Index Route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
@@ -5,9 +6,10 @@ function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
-
       <AppNav />
-      <p>list of cities</p>
+
+      <Outlet />
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
